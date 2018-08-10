381ddaf91e3accabf16c3a14f23998a000efeff1
Harald Kirschner <hkirschner@mozilla.com>
2018-08-09 21:46:10 -0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1 v-on:click="reverseMessage">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: () => {
+    return {
+      msg: 'Click Me!'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      console.log(this)
+      this.msg = this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  cursor: pointer;
+  text-decoration: underline;
+  font-weight: normal;
+}
+a {
+  color: #42b983;
+}
+</style>
